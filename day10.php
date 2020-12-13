8f6145467ba1fde68cb7aa5069119ac29be49add
Mark Bennett <mbennett@rawnet.com>
2020-12-13 10:44:28 +0000

    Day 10 part 2 - incomplete

--- day10.php
+++ day10.php
@@ -2,8 +2,8 @@
 
 include('common.php');
 
-// const INPUT_FILE = 'test.txt';
-const INPUT_FILE = 'day10-input.txt';
+const INPUT_FILE = 'test.txt';
+// const INPUT_FILE = 'day10-input.txt';
 
 $data = buildData(getInput(INPUT_FILE));
 
@@ -11,6 +11,41 @@ print 'Part 1: ' . (part1($data) ?? 'No answer') . "\n";
 print 'Part 2: ' . (part2($data) ?? 'No answer') . "\n";
 
 function part1(array $data): ?int
+{
+    $diffs = getDiffs($data);
+    return $diffs[1] * $diffs[3];
+}
+
+/**
+ * Works well for test cases, but too inefficient for actual problem
+ * @param array $data
+ * @return int
+ */
+function part2(array $data): ?int
+{
+    // Initialise with the first joltage
+    $chains = [reset($data)];
+
+    while (true) {
+        $added = 0;
+        foreach ($chains as $index => $value) {
+            $nextValues = getNextValues($data, $value);
+            $added += count($nextValues);
+            if (count($nextValues) === 0) {
+                continue;
+            }
+
+            $chains[$index] = array_shift($nextValues);
+            $chains = array_merge($chains, $nextValues);
+        }
+
+        if ($added === 0) {
+            return count($chains);
+        }
+    }
+}
+
+function getDiffs(array $data): array
 {
     $diffs = [];
 
@@ -24,14 +59,24 @@ function part1(array $data): ?int
         }
     }
 
-    return $diffs[1] * $diffs[3];
+    return $diffs;
 }
 
-function part2(array $data): ?int
+function getNextValues(array $data, int $value): array
 {
-    return null;
-}
+    $nextValues = [];
+    foreach ($data as $item) {
+        if ($item > $value && $item <= $value + 3) {
+            $nextValues[] = $item;
+        }
 
+        if ($item > $value + 3) {
+            return $nextValues;
+        }
+    }
+
+    return $nextValues;
+}
 
 function buildData(array $data): array
 {
